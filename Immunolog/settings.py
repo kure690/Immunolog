368622ca6dddc61be1898e3e7feba43c02ee21d6
kure690 <bobtani2234@gmail.com>
2024-06-28 15:43:51 +0800

    updated database

--- Immunolog/settings.py
+++ Immunolog/settings.py
@@ -77,10 +77,22 @@ WSGI_APPLICATION = 'Immunolog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Immunolog',
+        'USER': 'postgres',
+        'PASSWORD': 'Generic123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
