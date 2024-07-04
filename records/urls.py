118185f175fd22ae240f8a8e2cc0c87e200b89c0
kure690 <bobtani2234@gmail.com>
2024-07-04 18:49:07 +0800

    user dashboard and reject feature for admin

--- records/urls.py
+++ records/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import (AddVaccineRecord)
-from .views import accept_vaccine_record
+from .views import AddVaccineRecord
+from .views import accept_vaccine_record, reject_vaccine_record
 
 urlpatterns = [
    path('addrecord', AddVaccineRecord.as_view(), name='addrecord'),
    path('accept_vaccine_record/<int:pk>/', accept_vaccine_record, name='accept_vaccine_record'),
+   path('reject_vaccine_record/<int:pk>/', reject_vaccine_record, name='reject_vaccine_record')
 
 ]
